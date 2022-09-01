733b8c0bf1d3a26888e81c5df9f7d6062e941cba
Alejandro Castrillón <4lejo.castrillon@gmail.com>
2022-09-01 13:34:51 -0500

    Import created components and pages
    Add routes to pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AboutPageComponent } from './core/pages/about-page/about-page.component';
+import { PageNotFoundComponent } from '@core/pages/page-not-found/page-not-found.component';
+import { AboutPageComponent } from '@core/pages/about-page/about-page.component';
+import { UserProfilePageComponent } from '@features/auth/user-profile-page/user-profile-page.component';
+import { AuthGuard } from '@auth0/auth0-angular';
 
 const routes: Routes = [
-	{ path: 'heroes', loadChildren: () => import('./features/heroes/heroes.module').then(m => m.HeroesModule) },
 	{ path: 'about', component: AboutPageComponent },
-	{ path: '**', pathMatch: 'full', redirectTo: 'heroes' }
+	{ path: 'profile', component: UserProfilePageComponent, canActivate: [AuthGuard] },
+	{ path: 'heroes', loadChildren: () => import('@features/heroes/heroes.module').then(m => m.HeroesModule) },
+	{ path: '404', component: PageNotFoundComponent },
+	{ path: '', pathMatch: 'full', redirectTo: 'heroes' },
+	{ path: '**', pathMatch: 'full', redirectTo: '404' }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './core/components/navbar/navbar.component';
 import { AboutPageComponent } from './core/pages/about-page/about-page.component';
+import { PageNotFoundComponent } from './core/pages/page-not-found/page-not-found.component';
+import { AuthAreaComponent } from './core/components/auth-area/auth-area.component';
+import { UserProfilePageComponent } from './features/auth/user-profile-page/user-profile-page.component';
 
 @NgModule({
 	declarations: [
 		AppComponent,
 		NavbarComponent,
 		AboutPageComponent,
+		PageNotFoundComponent,
+		AuthAreaComponent,
+		UserProfilePageComponent,
 	],
 	imports: [
 		BrowserModule,
